5c6f6e1686cb4290b6b0403165e1783f3c2fbc00
Dmitriy Ryabov <ryabov.dmitriy@icloud.com>
2025-10-16 15:06:13 +0300

    Improved autostart mechanism

--- app/src/main/java/org/verba/BootReceiver.java
+++ app/src/main/java/org/verba/BootReceiver.java
@@ -6,9 +6,13 @@ import android.content.Intent;
 import android.os.Build;
 
 public class BootReceiver extends BroadcastReceiver {
+    private static final String ACTION_QUICKBOOT_POWERON = "android.intent.action.QUICKBOOT_POWERON";
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())
+                || Intent.ACTION_LOCKED_BOOT_COMPLETED.equals(intent.getAction())
+                || ACTION_QUICKBOOT_POWERON.equals(intent.getAction())) {
             Intent serviceIntent = new Intent(context, VoiceService.class);
             serviceIntent.setAction("org.verba.VOICE");
 
